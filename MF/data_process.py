8f2a27849d3f917cb56d3da225e72aa27cfaecdd
zhangqianjin <iamzhangqianjin@163.com>
2020-04-16 15:11:25 +0800

    Create data_process.py

---
+++ MF/data_process.py
@@ -0,0 +1,44 @@
+import torch
+from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
+import sys
+
+class feature_label_DataSet(Dataset):
+    def __init__(self, filename):
+        self.filename = filename
+        content_list = []
+        with open(filename) as f:
+            for line in f:
+                line = line.strip()
+                content_list.append(line)
+        self.content_info = content_list
+
+    def __len__(self):
+        return len(self.content_info)
+
+    def __getitem__(self, idx):
+        content = self.content_info[idx]
+        feature_label = content.split("\t")
+        feature_list = list(map(int,feature_label[:-1]))
+        label = feature_label[-1]
+        label = float(label)
+        return {"feature": feature_list, "label": label}
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    dataset = feature_label_DataSet(filename)
+    train_num = int(len(dataset) * 0.8)
+    test_num = len(dataset) - train_num
+    feature_tensor = torch.tensor([f["feature"] for f in dataset], dtype=torch.float)
+    label_tensor = torch.tensor([f["label"] for f in dataset], dtype=torch.int)
+    dataset_tensor = TensorDataset(feature_tensor, label_tensor)
+
+    train_set, test_set = random_split(dataset_tensor, (train_num, test_num))
+    train_dataloader = DataLoader(train_set, batch_size=4, shuffle=True)
+    test_dataloader = DataLoader(test_set, batch_size=4, shuffle=True)
+    for i_batch, sample_batched in enumerate(train_dataloader):
+        batch = tuple(t for t in sample_batched)
+        # batch = tuple(t.cuda() for t in sample_batched)
+        feature, label = batch
+        print(feature)
+        print(label.shape)
