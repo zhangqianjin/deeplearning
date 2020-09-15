f6a3417786b84ae5c2d8d869ca45758bb3eeb454
zhangqianjin <iamzhangqianjin@163.com>
2020-09-15 15:56:58 +0800

    Update read_data_train.py

--- tf2.0/read_data_train.py
+++ tf2.0/read_data_train.py
@@ -15,6 +15,18 @@ class myModel(tf.keras.Model):
         x = tf.math.reduce_mean(x, axis=1)
         x = self.Dense(x)
         return x
+    
+def input_fn(data_file, batch_size, num_epoch = None):
+    print("num_epoch=",num_epoch)
+    dataset = (tf.data.TextLineDataset(data_file))
+    dataset = dataset.map(lambda string: tf.strings.split(string, sep=","))
+    dataset = dataset.map(lambda string : tf.compat.v1.string_to_number(string,tf.int32))
+    dataset = dataset.map(lambda x: (x[:-1], x[-1]))
+    dataset = dataset.repeat(num_epoch)
+    dataset = dataset.batch(batch_size)
+    iterator = tf.compat.v1.data.make_one_shot_iterator(dataset)
+    return iterator 
+
 
 
 vocab_size = 10
@@ -22,23 +34,13 @@ vec_size = 128
 output_size = 10
 num_epochs = 10
 batch_size = 16
-
-model = myModel(vocab_size, output_size, vec_size)
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-
-
+mirrored_strategy = tf.distribute.MirroredStrategy()
+with mirrored_strategy.scope():
+    model = myModel(vocab_size, output_size, vec_size)
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
 filename = "t"
-#titanic_lines = tf.data.TextLineDataset(filename)
-def data_func(line):
-    line = tf.strings.split(line, sep = ",")
-    return line
-
-#titanic_data = titanic_lines.skip(1).map(data_func).map(lambda x: ).map(lambda x:(x[:-1], x[-1]))
-dataset = tf.data.TextLineDataset(filename)
-dataset = dataset.map(lambda string: tf.strings.split(string, sep=","))
-dataset = dataset.map(lambda string : tf.compat.v1.string_to_number(string,tf.int32))
-dataset = dataset.map(lambda x: (x[:-1], x[-1]))
-dataset = dataset.batch(batch_size=5)
-iterator = tf.compat.v1.data.make_one_shot_iterator(dataset)
-features, labels = iterator.get_next()
-model.fit(features, labels, epochs=5)
+iterator = input_fn(filename, batch_size=5, num_epoch = 10)
+for data in iterator:
+    feature, label = data
+    model.fit(feature, label, epochs=1, workers=1, batch_size=2)
