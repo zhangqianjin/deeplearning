c6120f3403b7a0ce63e68510df02294a54d6cbe5
zhangqianjin <iamzhangqianjin@163.com>
2021-01-29 15:44:54 +0800

    Update main.sh

--- language model/tf2.0/dnn/main.sh
+++ language model/tf2.0/dnn/main.sh
@@ -1,2 +1,3 @@
 python3 -u train/train.py  --data_path data/${datetime}  --model_path  model/${datetime}
 
+python3 -u predict/predict.py --gpu_num $value  --data_path data/${datetime} --model_path ../model/best_model/best_model --topN 500
